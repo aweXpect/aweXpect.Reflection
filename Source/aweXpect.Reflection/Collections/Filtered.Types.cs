1a503dac67afadbdb6d9126a6220563b318d0c6e
Valentin <valentin.breuss@baur.eu>
2025-04-09 12:22:48 +0200

    Review issues

--- Source/aweXpect.Reflection/Collections/Filtered.Types.cs
+++ Source/aweXpect.Reflection/Collections/Filtered.Types.cs
@@ -91,7 +91,10 @@ public static partial class Filtered
 			_description = events.GetDescription() + TypesSuffix;
 		}
 
-		protected Types(Types inner) : base(inner, inner.Filters)
+		/// <summary>
+		///     Container for a filterable collection of <see cref="Type" />.
+		/// </summary>
+		private Types(Types inner) : base(inner, inner.Filters)
 		{
 			_description = inner._description;
 			_assemblies = inner._assemblies;
@@ -143,115 +146,122 @@ public static partial class Filtered
 		///     Get all properties in the filtered types.
 		/// </summary>
 		public Properties Properties() => new(this, "properties ");
-		
-	public class StringEqualityResult : Types
-	{
-		private readonly StringEqualityOptions _options;
 
-		public StringEqualityResult(Types inner, StringEqualityOptions options) : base(inner)
+		/// <summary>
+		///     A Container for a filterable collection of <see cref="Type" />,
+		///     that also allows specifying string equality options.
+		/// </summary>
+		public class StringEqualityResult : Types
 		{
-			_options = options;
+			private readonly StringEqualityOptions _options;
+
+			internal StringEqualityResult(Types inner, StringEqualityOptions options) : base(inner)
+			{
+				_options = options;
+			}
+
+			/// <summary>
+			///     Ignores casing when comparing the <see langword="string" />s,
+			///     according to the <paramref name="ignoreCase" /> parameter.
+			/// </summary>
+			public StringEqualityResult IgnoringCase(bool ignoreCase = true)
+			{
+				_options.IgnoringCase(ignoreCase);
+				return this;
+			}
+
+			/// <summary>
+			///     Ignores leading white-space when comparing <see langword="string" />s,
+			///     according to the <paramref name="ignoreLeadingWhiteSpace" /> parameter.
+			/// </summary>
+			/// <remarks>
+			///     Note:<br />
+			///     This affects the index of first mismatch, as the removed whitespace is also ignored for the index calculation!
+			/// </remarks>
+			public StringEqualityResult IgnoringLeadingWhiteSpace(bool ignoreLeadingWhiteSpace = true)
+			{
+				_options.IgnoringLeadingWhiteSpace(ignoreLeadingWhiteSpace);
+				return this;
+			}
+
+			/// <summary>
+			///     Ignores trailing white-space when comparing <see langword="string" />s,
+			///     according to the <paramref name="ignoreTrailingWhiteSpace" /> parameter.
+			/// </summary>
+			public StringEqualityResult IgnoringTrailingWhiteSpace(bool ignoreTrailingWhiteSpace = true)
+			{
+				_options.IgnoringTrailingWhiteSpace(ignoreTrailingWhiteSpace);
+				return this;
+			}
+
+			/// <summary>
+			///     Uses the provided <paramref name="comparer" /> for comparing <see langword="string" />s.
+			/// </summary>
+			public StringEqualityResult Using(IEqualityComparer<string> comparer)
+			{
+				_options.UsingComparer(comparer);
+				return this;
+			}
 		}
 
 		/// <summary>
-		///     Ignores casing when comparing the <see langword="string" />s,
-		///     according to the <paramref name="ignoreCase" /> parameter.
+		///     A Container for a filterable collection of <see cref="Type" />,
+		///     that also allows specifying string equality options and types.
 		/// </summary>
-		public StringEqualityResult IgnoringCase(bool ignoreCase = true)
+		public class StringEqualityResultType : StringEqualityResult
 		{
-			_options.IgnoringCase(ignoreCase);
-			return this;
-		}
+			private readonly StringEqualityOptions _options;
 
-		/// <summary>
-		///     Ignores leading white-space when comparing <see langword="string" />s,
-		///     according to the <paramref name="ignoreLeadingWhiteSpace" /> parameter.
-		/// </summary>
-		/// <remarks>
-		///     Note:<br />
-		///     This affects the index of first mismatch, as the removed whitespace is also ignored for the index calculation!
-		/// </remarks>
-		public StringEqualityResult IgnoringLeadingWhiteSpace(bool ignoreLeadingWhiteSpace = true)
-		{
-			_options.IgnoringLeadingWhiteSpace(ignoreLeadingWhiteSpace);
-			return this;
-		}
+			internal StringEqualityResultType(Types inner, StringEqualityOptions options) : base(inner, options)
+			{
+				_options = options;
+			}
 
-		/// <summary>
-		///     Ignores trailing white-space when comparing <see langword="string" />s,
-		///     according to the <paramref name="ignoreTrailingWhiteSpace" /> parameter.
-		/// </summary>
-		public StringEqualityResult IgnoringTrailingWhiteSpace(bool ignoreTrailingWhiteSpace = true)
-		{
-			_options.IgnoringTrailingWhiteSpace(ignoreTrailingWhiteSpace);
-			return this;
-		}
+			/// <summary>
+			///     Interprets the expected <see langword="string" /> to be exactly equal.
+			/// </summary>
+			public StringEqualityResult Exactly()
+			{
+				_options.Exactly();
+				return this;
+			}
 
-		/// <summary>
-		///     Uses the provided <paramref name="comparer" /> for comparing <see langword="string" />s.
-		/// </summary>
-		public StringEqualityResult Using(IEqualityComparer<string> comparer)
-		{
-			_options.UsingComparer(comparer);
-			return this;
+			/// <summary>
+			///     Interprets the expected <see langword="string" /> as a prefix, so that the actual value starts with it.
+			/// </summary>
+			public StringEqualityResult AsPrefix()
+			{
+				_options.AsPrefix();
+				return this;
+			}
+
+			/// <summary>
+			///     Interprets the expected <see langword="string" /> as <see cref="Regex" /> pattern.
+			/// </summary>
+			public StringEqualityResult AsRegex()
+			{
+				_options.AsRegex();
+				return this;
+			}
+
+			/// <summary>
+			///     Interprets the expected <see langword="string" /> as a suffix, so that the actual value ends with it.
+			/// </summary>
+			public StringEqualityResult AsSuffix()
+			{
+				_options.AsSuffix();
+				return this;
+			}
+
+			/// <summary>
+			///     Interprets the expected <see langword="string" /> as wildcard pattern.<br />
+			///     Supports * to match zero or more characters and ? to match exactly one character.
+			/// </summary>
+			public StringEqualityResult AsWildcard()
+			{
+				_options.AsWildcard();
+				return this;
+			}
 		}
 	}
-
-	public class StringEqualityResultType : StringEqualityResult
-	{
-		private readonly StringEqualityOptions _options;
-
-		public StringEqualityResultType(Types inner, StringEqualityOptions options) : base(inner, options)
-		{
-			_options = options;
-		}
-		
-		/// <summary>
-		///     Interprets the expected <see langword="string" /> to be exactly equal.
-		/// </summary>
-		public StringEqualityResult Exactly()
-		{
-			_options.Exactly();
-			return this;
-		}
-
-		/// <summary>
-		///     Interprets the expected <see langword="string" /> as a prefix, so that the actual value starts with it.
-		/// </summary>
-		public StringEqualityResult AsPrefix()
-		{
-			_options.AsPrefix();
-			return this;
-		}
-
-		/// <summary>
-		///     Interprets the expected <see langword="string" /> as <see cref="Regex" /> pattern.
-		/// </summary>
-		public StringEqualityResult AsRegex()
-		{
-			_options.AsRegex();
-			return this;
-		}
-
-		/// <summary>
-		///     Interprets the expected <see langword="string" /> as a suffix, so that the actual value ends with it.
-		/// </summary>
-		public StringEqualityResult AsSuffix()
-		{
-			_options.AsSuffix();
-			return this;
-		}
-
-		/// <summary>
-		///     Interprets the expected <see langword="string" /> as wildcard pattern.<br />
-		///     Supports * to match zero or more characters and ? to match exactly one character.
-		/// </summary>
-		public StringEqualityResult AsWildcard()
-		{
-			_options.AsWildcard();
-			return this;
-		}
-	}
-	}
-
 }
