a06f65c4bce1c0b1afe5df43afb198a65d7cca85
Valentin Breuß <vbreuss@gmail.com>
2025-04-19 07:16:56 +0200

    feat: add `OrWith` for attribute filters on `MethodInfo`

--- Tests/aweXpect.Reflection.Tests/Collections/FilteredExtensions.Methods.With.AttributeTests.cs
+++ Tests/aweXpect.Reflection.Tests/Collections/FilteredExtensions.Methods.With.AttributeTests.cs
@@ -12,7 +12,7 @@ public sealed partial class FilteredExtensions
 			public sealed class AttributeTests
 			{
 				[Fact]
-				public async Task ShouldFilterForTypesWithAttribute()
+				public async Task ShouldFilterForMethodsWithAttribute()
 				{
 					Reflection.Collections.Filtered.Methods methods = In.AssemblyContaining<FilteredExtensions>()
 						.Types().Methods().With<BarAttribute>();
@@ -21,7 +21,9 @@ public sealed partial class FilteredExtensions
 						typeof(Dummy).GetMethod(nameof(Dummy.MyBarMethod)),
 						typeof(DummyChild).GetMethod(nameof(DummyChild.MyBarMethod)),
 					]).InAnyOrder();
-					await That(methods.GetDescription()).IsEqualTo("methods with FilteredExtensions.Methods.With.AttributeTests.BarAttribute").AsPrefix();
+					await That(methods.GetDescription())
+						.IsEqualTo("methods with FilteredExtensions.Methods.With.BarAttribute")
+						.AsPrefix();
 				}
 
 				[Fact]
@@ -31,7 +33,9 @@ public sealed partial class FilteredExtensions
 						.Types().Methods().With<BarAttribute>(false);
 
 					await That(methods).HasSingle().Which.IsEqualTo(typeof(Dummy).GetMethod(nameof(Dummy.MyBarMethod)));
-					await That(methods.GetDescription()).IsEqualTo("methods with direct FilteredExtensions.Methods.With.AttributeTests.BarAttribute").AsPrefix();
+					await That(methods.GetDescription())
+						.IsEqualTo("methods with direct FilteredExtensions.Methods.With.BarAttribute")
+						.AsPrefix();
 				}
 
 				[Theory]
@@ -44,7 +48,9 @@ public sealed partial class FilteredExtensions
 
 					await That(methods).IsEqualTo(expectedTypes).InAnyOrder();
 					await That(methods.GetDescription())
-						.IsEqualTo("methods with FilteredExtensions.Methods.With.AttributeTests.FooAttribute matching foo => foo.Value == value").AsPrefix();
+						.IsEqualTo(
+							"methods with FilteredExtensions.Methods.With.FooAttribute matching foo => foo.Value == value")
+						.AsPrefix();
 				}
 
 				[Fact]
@@ -56,47 +62,9 @@ public sealed partial class FilteredExtensions
 					await That(methods).HasSingle().Which
 						.IsEqualTo(typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod2)));
 					await That(methods.GetDescription())
-						.IsEqualTo("methods with direct FilteredExtensions.Methods.With.AttributeTests.FooAttribute matching foo => foo.Value == 2").AsPrefix();
-				}
-
-				[AttributeUsage(AttributeTargets.Method)]
-				private class BarAttribute : Attribute
-				{
-				}
-
-				private class Dummy
-				{
-					[Bar]
-					public virtual void MyBarMethod()
-					{
-					}
-
-					[Foo(Value = 2)]
-					public virtual void MyFooMethod2()
-					{
-					}
-
-					[Foo(Value = 3)]
-					public virtual void MyFooMethod3()
-					{
-					}
-				}
-
-				private class DummyChild : Dummy
-				{
-					public override void MyBarMethod()
-					{
-					}
-
-					public override void MyFooMethod2()
-					{
-					}
-				}
-
-				[AttributeUsage(AttributeTargets.Method)]
-				private class FooAttribute : Attribute
-				{
-					public int Value { get; set; }
+						.IsEqualTo(
+							"methods with direct FilteredExtensions.Methods.With.FooAttribute matching foo => foo.Value == 2")
+						.AsPrefix();
 				}
 
 				public static TheoryData<int, MethodInfo?[]> GetFooValues()
@@ -112,6 +80,134 @@ public sealed partial class FilteredExtensions
 							3, [typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod3)),]
 						},
 					};
+			}
+
+			public sealed class OrWithAttributeTests
+			{
+				[Fact]
+				public async Task ShouldFilterForMethodsWithAttribute()
+				{
+					Reflection.Collections.Filtered.Methods methods = In.AssemblyContaining<FilteredExtensions>()
+						.Types().Methods().With<BarAttribute>().OrWith<FooAttribute>();
+
+					await That(methods).IsEqualTo([
+						typeof(Dummy).GetMethod(nameof(Dummy.MyBarMethod)),
+						typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod2)),
+						typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod3)),
+						typeof(DummyChild).GetMethod(nameof(DummyChild.MyBarMethod)),
+						typeof(DummyChild).GetMethod(nameof(DummyChild.MyFooMethod2)),
+					]).InAnyOrder();
+					await That(methods.GetDescription())
+						.IsEqualTo(
+							"methods with FilteredExtensions.Methods.With.BarAttribute or with FilteredExtensions.Methods.With.FooAttribute")
+						.AsPrefix();
+				}
+
+				[Fact]
+				public async Task WhenInheritIsSetToFalse_ShouldFilterForTypesWithAttributeDirectlySet()
+				{
+					Reflection.Collections.Filtered.Methods methods = In.AssemblyContaining<FilteredExtensions>()
+						.Types().Methods().With<BarAttribute>().OrWith<FooAttribute>(false);
+
+					await That(methods).IsEqualTo([
+						typeof(Dummy).GetMethod(nameof(Dummy.MyBarMethod)),
+						typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod2)),
+						typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod3)),
+						typeof(DummyChild).GetMethod(nameof(DummyChild.MyBarMethod)),
+					]).InAnyOrder();
+					await That(methods.GetDescription())
+						.IsEqualTo(
+							"methods with FilteredExtensions.Methods.With.BarAttribute or with direct FilteredExtensions.Methods.With.FooAttribute")
+						.AsPrefix();
+				}
+
+				[Theory]
+				[MemberData(nameof(GetFooValues))]
+				public async Task WithPredicate_ShouldFilterForTypesWithAttributeMatchingPredicate(int value,
+					MethodInfo?[] expectedTypes)
+				{
+					Reflection.Collections.Filtered.Methods methods = In.AssemblyContaining<FilteredExtensions>()
+						.Types().Methods().With<BarAttribute>(false).OrWith<FooAttribute>(foo => foo.Value == value);
+
+					await That(methods).IsEqualTo(expectedTypes).InAnyOrder();
+					await That(methods.GetDescription())
+						.IsEqualTo(
+							"methods with direct FilteredExtensions.Methods.With.BarAttribute or with FilteredExtensions.Methods.With.FooAttribute matching foo => foo.Value == value")
+						.AsPrefix();
+				}
+
+				[Fact]
+				public async Task WithPredicate_WhenInheritIsSetToFalse_ShouldFilterForTypesWithAttributeDirectlySet()
+				{
+					Reflection.Collections.Filtered.Methods methods = In.AssemblyContaining<FilteredExtensions>()
+						.Types().Methods().With<BarAttribute>(_ => false)
+						.OrWith<FooAttribute>(foo => foo.Value == 2, false);
+
+					await That(methods).HasSingle().Which
+						.IsEqualTo(typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod2)));
+					await That(methods.GetDescription())
+						.IsEqualTo(
+							"methods with FilteredExtensions.Methods.With.BarAttribute matching _ => false or with direct FilteredExtensions.Methods.With.FooAttribute matching foo => foo.Value == 2")
+						.AsPrefix();
+				}
+
+				public static TheoryData<int, MethodInfo?[]> GetFooValues()
+					=> new()
+					{
+						{
+							2, [
+								typeof(Dummy).GetMethod(nameof(Dummy.MyBarMethod)),
+								typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod2)),
+								typeof(DummyChild).GetMethod(nameof(DummyChild.MyFooMethod2)),
+							]
+						},
+						{
+							3, [
+								typeof(Dummy).GetMethod(nameof(Dummy.MyBarMethod)),
+								typeof(Dummy).GetMethod(nameof(Dummy.MyFooMethod3)),
+							]
+						},
+					};
+			}
+
+			[AttributeUsage(AttributeTargets.Method)]
+			private class BarAttribute : Attribute
+			{
+			}
+
+			private class Dummy
+			{
+				[Bar]
+				public virtual void MyBarMethod()
+				{
+				}
+
+				[Foo(Value = 2)]
+				public virtual void MyFooMethod2()
+				{
+				}
+
+				[Foo(Value = 3)]
+				public virtual void MyFooMethod3()
+				{
+				}
+			}
+
+			private class DummyChild : Dummy
+			{
+				public override void MyBarMethod()
+				{
+				}
+
+				public override void MyFooMethod2()
+				{
+				}
+			}
+
+			[AttributeUsage(AttributeTargets.Method)]
+			private class FooAttribute : Attribute
+			{
+				public int Value { get; set; }
 			}
 		}
 	}
