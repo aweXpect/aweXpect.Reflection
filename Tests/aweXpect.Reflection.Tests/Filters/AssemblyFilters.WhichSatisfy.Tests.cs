9488605e791028fd8443abba347b57e36b504500
Valentin Breuß <vbreuss@gmail.com>
2025-07-27 10:56:15 +0200

    Fix sonar issue

--- Tests/aweXpect.Reflection.Tests/Filters/AssemblyFilters.WhichSatisfy.Tests.cs
+++ Tests/aweXpect.Reflection.Tests/Filters/AssemblyFilters.WhichSatisfy.Tests.cs
@@ -12,12 +12,12 @@ public sealed partial class AssemblyFilters
 			public async Task ShouldFilterForAssembliesWhichSatisfyThePredicate()
 			{
 				Filtered.Assemblies assemblies = In.AllLoadedAssemblies()
-					.WhichSatisfy(it => it.FullName.Contains("aweXpect.Reflection.Tests"));
+					.WhichSatisfy(it => it.FullName!.Contains("aweXpect.Reflection.Tests"));
 
 				await That(assemblies).HasSingle().Which.IsEqualTo(typeof(AssemblyFilters).Assembly);
 				await That(assemblies.GetDescription())
 					.IsEqualTo(
-						"in all loaded assemblies matching it => it.FullName.Contains(\"aweXpect.Reflection.Tests\")");
+						"in all loaded assemblies matching it => it.FullName!.Contains(\"aweXpect.Reflection.Tests\")");
 			}
 		}
 	}
