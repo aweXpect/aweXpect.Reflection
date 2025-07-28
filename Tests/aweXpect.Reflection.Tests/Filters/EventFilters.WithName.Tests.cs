01349d1390c452448abaaa1a4cffe892c874861c
Breuß Valentin <valentin.breuss@baur.eu>
2025-07-28 12:39:19 +0200

    feat: add `In.Type{s}` to directly create expectation on types
    ---
    - Fixes #51

--- Tests/aweXpect.Reflection.Tests/Filters/EventFilters.WithName.Tests.cs
+++ Tests/aweXpect.Reflection.Tests/Filters/EventFilters.WithName.Tests.cs
@@ -12,92 +12,92 @@ public sealed partial class EventFilters
 			[Fact]
 			public async Task ShouldFilterForTypesWithName()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName(nameof(SomeClassToVerifyTheEventNameOfIt.SomeEventToVerifyTheNameOfIt));
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
-					.IsEqualTo("events with name equal to \"SomeEventToVerifyTheNameOfIt\" in assembly")
+					.IsEqualTo("events with name equal to \"SomeEventToVerifyTheNameOfIt\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsPrefix()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName("SomeEventToVerifyTheNameOf").AsPrefix();
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
-					.IsEqualTo("events with name starting with \"SomeEventToVerifyTheNameOf\" in assembly")
+					.IsEqualTo("events with name starting with \"SomeEventToVerifyTheNameOf\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsRegex()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName("[a-zA-Z]*EventToVerifyTheName").AsRegex();
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
-					.IsEqualTo("events with name matching regex \"[a-zA-Z]*EventToVerifyTheName\" in assembly")
+					.IsEqualTo("events with name matching regex \"[a-zA-Z]*EventToVerifyTheName\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsSuffix()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName("EventToVerifyTheNameOfIt").AsSuffix();
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
-					.IsEqualTo("events with name ending with \"EventToVerifyTheNameOfIt\" in assembly").AsPrefix();
+					.IsEqualTo("events with name ending with \"EventToVerifyTheNameOfIt\" in").AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsWildcard()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName("*EventToVerifyTheNameOf*").AsWildcard();
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
-					.IsEqualTo("events with name matching \"*EventToVerifyTheNameOf*\" in assembly").AsPrefix();
+					.IsEqualTo("events with name matching \"*EventToVerifyTheNameOf*\" in").AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportExactly()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName(nameof(SomeClassToVerifyTheEventNameOfIt.SomeEventToVerifyTheNameOfIt))
 					.Exactly();
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
-					.IsEqualTo("events with name equal to \"SomeEventToVerifyTheNameOfIt\" in assembly")
+					.IsEqualTo("events with name equal to \"SomeEventToVerifyTheNameOfIt\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportIgnoringCase()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName(nameof(SomeClassToVerifyTheEventNameOfIt.SomeEventToVerifyTheNameOfIt)
 						.ToLowerInvariant()).IgnoringCase();
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
 					.IsEqualTo(
-						"events with name equal to \"someeventtoverifythenameofit\" ignoring case in assembly")
+						"events with name equal to \"someeventtoverifythenameofit\" ignoring case in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportIgnoringLeadingWhiteSpace()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName(
 						"\t " + nameof(SomeClassToVerifyTheEventNameOfIt.SomeEventToVerifyTheNameOfIt))
 					.IgnoringLeadingWhiteSpace();
@@ -105,14 +105,14 @@ public sealed partial class EventFilters
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
 					.IsEqualTo(
-						"events with name equal to \"\\t SomeEventToVerifyTheNameOfIt\" ignoring leading white-space in assembly")
+						"events with name equal to \"\\t SomeEventToVerifyTheNameOfIt\" ignoring leading white-space in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportIgnoringTrailingWhiteSpace()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName(
 						nameof(SomeClassToVerifyTheEventNameOfIt.SomeEventToVerifyTheNameOfIt) + "\t ")
 					.IgnoringTrailingWhiteSpace();
@@ -120,21 +120,21 @@ public sealed partial class EventFilters
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
 					.IsEqualTo(
-						"events with name equal to \"SomeEventToVerifyTheNameOfIt\\t \" ignoring trailing white-space in assembly")
+						"events with name equal to \"SomeEventToVerifyTheNameOfIt\\t \" ignoring trailing white-space in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportUsingCustomComparer()
 			{
-				Filtered.Events events = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Events events = In.Type<SomeClassToVerifyTheEventNameOfIt>()
 					.Events().WithName("SOmEEventTOVErIfyThENAmEofit")
 					.Using(new IgnoreCaseForVocalsComparer());
 
 				await That(events).HasSingle().Which.IsEqualTo(ExpectedEventInfo());
 				await That(events.GetDescription())
 					.IsEqualTo(
-						"events with name equal to \"SOmEEventTOVErIfyThENAmEofit\" using IgnoreCaseForVocalsComparer in assembly")
+						"events with name equal to \"SOmEEventTOVErIfyThENAmEofit\" using IgnoreCaseForVocalsComparer in")
 					.AsPrefix();
 			}
 		}
--- Tests/aweXpect.Reflection.Tests/Filters/PropertyFilters.WithName.Tests.cs
+++ Tests/aweXpect.Reflection.Tests/Filters/PropertyFilters.WithName.Tests.cs
@@ -12,92 +12,92 @@ public sealed partial class PropertyFilters
 			[Fact]
 			public async Task ShouldFilterForTypesWithName()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName(nameof(SomeClassToVerifyThePropertyNameOfIt.SomePropertyToVerifyTheNameOfIt));
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
-					.IsEqualTo("properties with name equal to \"SomePropertyToVerifyTheNameOfI…\" in assembly")
+					.IsEqualTo("properties with name equal to \"SomePropertyToVerifyTheNameOfI…\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsPrefix()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName("SomePropertyToVerifyTheNameOf").AsPrefix();
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
-					.IsEqualTo("properties with name starting with \"SomePropertyToVerifyTheNameOf\" in assembly")
+					.IsEqualTo("properties with name starting with \"SomePropertyToVerifyTheNameOf\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsRegex()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName("[a-zA-Z]*PropertyToVerifyTheName").AsRegex();
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
-					.IsEqualTo("properties with name matching regex \"[a-zA-Z]*PropertyToVerifyTheNa…\" in assembly")
+					.IsEqualTo("properties with name matching regex \"[a-zA-Z]*PropertyToVerifyTheNa…\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsSuffix()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName("PropertyToVerifyTheNameOfIt").AsSuffix();
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
-					.IsEqualTo("properties with name ending with \"PropertyToVerifyTheNameOfIt\" in assembly").AsPrefix();
+					.IsEqualTo("properties with name ending with \"PropertyToVerifyTheNameOfIt\" in").AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportAsWildcard()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName("*PropertyToVerifyTheNameOf*").AsWildcard();
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
-					.IsEqualTo("properties with name matching \"*PropertyToVerifyTheNameOf*\" in assembly").AsPrefix();
+					.IsEqualTo("properties with name matching \"*PropertyToVerifyTheNameOf*\" in").AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportExactly()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName(nameof(SomeClassToVerifyThePropertyNameOfIt.SomePropertyToVerifyTheNameOfIt))
 					.Exactly();
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
-					.IsEqualTo("properties with name equal to \"SomePropertyToVerifyTheNameOfI…\" in assembly")
+					.IsEqualTo("properties with name equal to \"SomePropertyToVerifyTheNameOfI…\" in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportIgnoringCase()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName(nameof(SomeClassToVerifyThePropertyNameOfIt.SomePropertyToVerifyTheNameOfIt)
 						.ToLowerInvariant()).IgnoringCase();
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
 					.IsEqualTo(
-						"properties with name equal to \"somepropertytoverifythenameofi…\" ignoring case in assembly")
+						"properties with name equal to \"somepropertytoverifythenameofi…\" ignoring case in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportIgnoringLeadingWhiteSpace()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName(
 						"\t " + nameof(SomeClassToVerifyThePropertyNameOfIt.SomePropertyToVerifyTheNameOfIt))
 					.IgnoringLeadingWhiteSpace();
@@ -105,14 +105,14 @@ public sealed partial class PropertyFilters
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
 					.IsEqualTo(
-						"properties with name equal to \"\\t SomePropertyToVerifyTheNameO…\" ignoring leading white-space in assembly")
+						"properties with name equal to \"\\t SomePropertyToVerifyTheNameO…\" ignoring leading white-space in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportIgnoringTrailingWhiteSpace()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName(
 						nameof(SomeClassToVerifyThePropertyNameOfIt.SomePropertyToVerifyTheNameOfIt) + "\t ")
 					.IgnoringTrailingWhiteSpace();
@@ -120,21 +120,21 @@ public sealed partial class PropertyFilters
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
 					.IsEqualTo(
-						"properties with name equal to \"SomePropertyToVerifyTheNameOfI…\" ignoring trailing white-space in assembly")
+						"properties with name equal to \"SomePropertyToVerifyTheNameOfI…\" ignoring trailing white-space in")
 					.AsPrefix();
 			}
 
 			[Fact]
 			public async Task ShouldSupportUsingCustomComparer()
 			{
-				Filtered.Properties properties = In.AssemblyContaining<AssemblyFilters>()
+				Filtered.Properties properties = In.Type<SomeClassToVerifyThePropertyNameOfIt>()
 					.Properties().WithName("SOmEPropertyTOVErIfyThENAmEofit")
 					.Using(new IgnoreCaseForVocalsComparer());
 
 				await That(properties).HasSingle().Which.IsEqualTo(ExpectedPropertyInfo());
 				await That(properties.GetDescription())
 					.IsEqualTo(
-						"properties with name equal to \"SOmEPropertyTOVErIfyThENAmEofi…\" using IgnoreCaseForVocalsComparer in assembly")
+						"properties with name equal to \"SOmEPropertyTOVErIfyThENAmEofi…\" using IgnoreCaseForVocalsComparer in")
 					.AsPrefix();
 			}
 		}
