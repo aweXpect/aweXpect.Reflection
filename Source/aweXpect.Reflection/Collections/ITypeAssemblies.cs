79a22bb9f0c67a613bca36df9bfb4faed29d8dac
Breuß Valentin <valentin.breuss@baur.eu>
2025-07-28 15:59:00 +0200

    feat: add access modifier properties to filtered assemblies
    --
    - *Fixes #52*

--- Source/aweXpect.Reflection/Collections/ITypeAssemblies.cs
+++ Source/aweXpect.Reflection/Collections/ITypeAssemblies.cs
@@ -33,4 +33,34 @@ public interface ITypeAssemblies : ILimitedTypeAssemblies<ITypeAssemblies>
 	///     Get all enums in the filtered assemblies.
 	/// </summary>
 	Filtered.Types Enums(AccessModifiers accessModifier = AccessModifiers.Any);
+
+	/// <summary>
+	///     An interface to allow filtering for types in assemblies.
+	/// </summary>
+	/// <remarks>
+	///     In addition to the properties and methods in <see cref="ITypeAssemblies" /> it also
+	///     allows specifying a private protected access modifier.
+	/// </remarks>
+	public interface IPrivate : ITypeAssemblies
+	{
+		/// <summary>
+		///     Filters only for private protected types.
+		/// </summary>
+		ITypeAssemblies Protected { get; }
+	}
+
+	/// <summary>
+	///     An interface to allow filtering for types in assemblies.
+	/// </summary>
+	/// <remarks>
+	///     In addition to the properties and methods in <see cref="ITypeAssemblies" /> it also
+	///     allows specifying a protected internal access modifier.
+	/// </remarks>
+	public interface IProtected : ITypeAssemblies
+	{
+		/// <summary>
+		///     Filters only for protected internal types.
+		/// </summary>
+		ITypeAssemblies Internal { get; }
+	}
 }
